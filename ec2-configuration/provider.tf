7db227f2a5b22ccb5749178bf876d8cc2dbb33a7
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-01-27 22:11:11 -0600

    terraform-ec2

--- ec2-configuration/provider.tf
+++ ec2-configuration/provider.tf
@@ -1,6 +1,16 @@
+terraform {
+    required_version = ">= 1.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
 # Configuración del proveedor AWS
 provider "aws" {
   region  = "us-east-1"             # Región de AWS donde se crean los recursos
-  profile = "<nombre-del-perfil>"   # Nombre del perfil configurado en AWS CLI con IAM Identity Center
+  profile = "srojas"                # Nombre del perfil configurado en AWS CLI con IAM Identity Center
 }
 
